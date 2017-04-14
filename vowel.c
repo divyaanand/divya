e21f7fd75b667554c0ddcb9825103e1e03bfd540
divyaanand <divyaanand1000@gmail.com>
2017-04-14 14:13:48 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,7 +3,7 @@ void main()
 {
 char ch;
 scanf("%d",&ch);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='I'||ch=='i'||ch=='O'||ch=='o'||ch=='U'||ch=='u')
 printf("vowels");
 else
 printf("consonent");
