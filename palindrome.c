4bb3c097fd9b35a043a09c25413a1957a4e5e51e
divyaanand <divyaanand1000@gmail.com>
2017-04-15 10:37:32 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int sum=0,num,r;
-printf("enter the number");
+int sum=0,num,r,temp;
 scanf("%d",&num);
+temp=num;
 while(num>0)
 {
 r=num%10;
-n=num/10;
+num=num/10;
 sum=(sum*10)+r;
 }
-if(num==sum)
+if(temp==num)
 {
 printf("palindrome");
 }
