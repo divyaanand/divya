1bdd860480e534c6a826b9bf120cf4d6fe5817a1
divyaanand <divyaanand1000@gmail.com>
2017-06-08 16:36:42 +0530

    Create fibb.java

---
+++ fibb.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Fibb{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int n,n1=0,n2=1,n3;
+n=in.nextInt();
+for(i=1;i<n;i++)
+{
+System.out.println(n1);
+}
+n1=n2;
+n2=n3;
+n3=n1+n2;
+System.out.println(n3);
+}
+}
